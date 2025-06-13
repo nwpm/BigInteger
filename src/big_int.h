cf4da17b6189ae045cc94e2970168949cc80fcec
nwpm <fwxgkt@gmail.com>
2025-06-13 19:36:46 +0300

    feat, refactor: change modificator of param, add new func
    big_int.h
    Add new functions:
    -bigint_is_equal
    -bigint_multiply
    -bigint_sum_in_param
    
    Add const modificator in all param

--- src/big_int.h
+++ src/big_int.h
@@ -16,13 +16,16 @@ typedef struct BigInt {
 BigInt *bigint_create();
 BigInt *bigint_create_from_num(long long num);
 BigInt *bigint_create_from_string(const char *str);
-BigInt *bigint_create_copy(BigInt *num);
+BigInt *bigint_create_copy(const BigInt *num);
 
-bool bigint_less(BigInt *n1, BigInt *n2);
+bool bigint_less(const BigInt *n1, const BigInt *n2);
+bool bigint_is_equal(const BigInt *n1, const BigInt *n2);
 
-BigInt *bigint_sum(BigInt *n1, BigInt *n2);
-BigInt *bigint_difference(BigInt *n1, BigInt *n2);
+BigInt *bigint_sum(const BigInt *n1, const BigInt *n2);
+BigInt *bigint_difference(const BigInt *n1, const BigInt *n2);
+BigInt *bigint_multiply(const BigInt *n1, unsigned int n2);
 
+void bigint_sum_in_param(BigInt **n1, const BigInt *n2);
 void bigint_free(BigInt *big_int);
 
 #endif // BIG_INT_H
